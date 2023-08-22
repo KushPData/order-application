e4e54300d0d436b075a37beb263925f5c6a1cc1e
KushPData <kushp03@uw.edu>
2023-08-22 16:29:16 +0530

    added css to the order list table

--- src/components/index.js
+++ src/components/index.js
@@ -7,14 +7,20 @@ if (sessionStorage.length !== 1) {
 
 let row = "";
 
-row += "<tr><th>P.O. Number</th><th>Date</th><th>Vendor</th><th>Ship To</th></tr>";
+row += "<tr><th>P.O. Number</th><th>Date</th><th>Product(s)</th><th>Vendor</th><th>Ship To</th></tr>";
 
 let keys = Object.keys(sessionStorage);
 for (let key of keys) {
     if (key !== "IsThisFirstTime_Log_From_LiveServer") {
         console.log(typeof JSON.parse(sessionStorage.getItem(key)));
         console.log(JSON.parse(sessionStorage.getItem(key))[0].date);
-        row += "<tr><td>" + key + "</td><td>" + JSON.parse(sessionStorage.getItem(key))[0].date + "</td><td>" + JSON.parse(sessionStorage.getItem(key))[0].vendor + "</td><td>" + JSON.parse(sessionStorage.getItem(key))[0].shipTo + "</td></tr>"
+        row += "<tr><td>" + key + "</td><td>" + JSON.parse(sessionStorage.getItem(key))[0].date + "</td>"
+
+        for(let i = 0; i < JSON.parse(sessionStorage.getItem(key))[0].nameArray.length; i++) {
+            row += "<td class='block'>" + JSON.parse(sessionStorage.getItem(key))[0].nameArray[i] + "</td>"
+        }
+        
+        row += "<td>" + JSON.parse(sessionStorage.getItem(key))[0].vendor + "</td><td>" + JSON.parse(sessionStorage.getItem(key))[0].shipTo + "</td></tr>"
     }
 }
 
